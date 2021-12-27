986f71c494d371c26caa126d68d34303dde8ec61
Vlad <VladBashlakov@gmail.com>
2021-12-27 20:19:45 +0300

    Initial commit

---
+++ src/main/java/com/example/secondcourse/SecondCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.secondcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecondCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/secondcourse/SecondCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secondcourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
